071294e4c0192d0f9905f362e636c74292b5b3ed
By-pa <by.pa28@gmail.com>
2023-09-20 21:10:07 +0300

    Начало проекта. Создана основная страница с товарами. Созданы карточки таваров в отдельном представлении Создана корзина Создан скелет оформления заказов

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using OnlineShopWebApp.Models;
+using OnlineShopWebApp;
+
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductsRepository productRepository;
+        private readonly ICartsRepository cartsRepository;
+
+        public CartController(IProductsRepository productRepository, ICartsRepository cartsRepository)
+        {
+            this.productRepository = productRepository;
+            this.cartsRepository = cartsRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var cart = cartsRepository.TryGetByUserId(Constants.UserId);
+            return View(cart);
+        }
+
+        public IActionResult Add(int productId)
+        {
+            var product = productRepository.TryGetById(productId);
+            cartsRepository.Add(product, Constants.UserId);
+            return RedirectToAction("Index");
+        }
+		public IActionResult DecreaseAmount(int productId)
+		{
+			cartsRepository.DecreaseAmount(productId, Constants.UserId);
+			return RedirectToAction("Index");
+		}
+		public IActionResult Clear()
+		{
+            cartsRepository.Clear(Constants.UserId);
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        
+        public User(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+    }
+}
